755a020470b4b245a8fb558f486baae139d472e7
John Khvatov <ivaxer@gmail.com>
2012-12-21 14:12:19 +0400

    Client: reuse connections to riak

--- riak/client.go
+++ riak/client.go
@@ -1,7 +1,13 @@
 package riak
 
+import (
+    "sync"
+)
+
 type Client struct {
     addr string
+    mu sync.Mutex
+    free_conns []*connection
 }
 
 func New(addr string) (client *Client) {
@@ -13,9 +19,21 @@ func (client *Client) Bucket(bucketname string) (bucket *Bucket) {
 }
 
 func (client *Client) getConnection() (conn *connection) {
+    client.mu.Lock()
+    defer client.mu.Unlock()
+
+    if len(client.free_conns) != 0 {
+        conn := client.free_conns[0]
+        client.free_conns = client.free_conns[1:]
+        return conn
+    }
+
     return newConnection(client.addr)
 }
 
 func (client *Client) releaseConnection(conn *connection) {
-    conn.Close()
+    client.mu.Lock()
+    defer client.mu.Unlock()
+
+    client.free_conns = append(client.free_conns, conn)
 }
